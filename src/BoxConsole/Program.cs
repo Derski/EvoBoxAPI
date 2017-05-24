7f25c3a86590a58605abea3c1f6ff96c14b50215
Derski <derkowski@gmail.com>
2017-05-24 15:52:19 -0600

    initial commit

---
+++ src/BoxConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EvoBoxAPI;
+using Box.V2;
+using Box.V2.Auth;
+using Box.V2.Config;
+using Box.V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace BoxConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task<BoxFile> task = BoxService.ExecuteMainAsyncFileUpload();
+            var awaiter = task.GetAwaiter();
+            
+            awaiter.OnCompleted(()=> OnFileCreatedComplete(awaiter.GetResult()));
+
+            //timer
+            for(int i = 0; i<100;i++)
+            {
+                Thread.Sleep(5000);
+            }
+        }
+
+        private static void OnFileCreatedComplete(BoxFile f)
+        {
+
+        }
+    }
+}
---
+++ src/EvoBoxAPI/BoxService.cs
@@ -0,0 +1,51 @@
+﻿using Box.V2;
+using Box.V2.Auth;
+using Box.V2.Config;
+using Box.V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvoBoxAPI
+{
+    public static class BoxService
+    {
+        
+        public static async Task<BoxFile> ExecuteMainAsyncFileUpload()
+        {
+            var accessToken = "hL80NM47Rv2OKCpgbJg7jzwe0DRck3ME";
+
+            var fileName = "RemoteFileName";
+
+            var localFilePath = "C:\\Users\\cderkowski\\Desktop\\Surface Display Software.pdf";
+
+            var parentFolderId = "0";//this should be root folder
+
+            //var timer = Stopwatch.StartNew();
+
+            var refreshToken = "";
+            var auth = new OAuthSession(accessToken, refreshToken, 3600, "bearer");
+
+            var clientID = "0g6avu3n8udr3pfow3zlfpcu9i9e71tk";
+
+            var config = new BoxConfig(clientID, clientID, new Uri("http://boxsdk"));
+            var client = new BoxClient(config, auth);
+            var file = File.OpenRead(localFilePath);
+            var fileRequest = new BoxFileRequest
+            {
+                Name = fileName,
+                Parent = new BoxFolderRequest { Id = parentFolderId }
+            };
+
+            var bFile = await client.FilesManager.UploadAsync(fileRequest, file);
+
+            return bFile;
+           
+        }
+    }
+
+
+}
